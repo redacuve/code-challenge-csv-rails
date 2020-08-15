e1134940336fc41e276b5e02fd64b63530447d88
Rey David Cuevas <cuevas.vela@gmail.com>
2020-08-15 12:55:58 -0500

    añadido multipart en el form para leer el archivo en el metodo_procesar_csv

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,5 +8,11 @@ class StaticPagesController < ApplicationController
 
   def procesar_csv
     puts "PROCESANDO..."
+    archivo = params[:archivo].tempfile
+    content_type = params[:archivo].content_type
+    puts params[:archivo].content_type
+    if archivo.respond_to?(:read) && content_type == 'text/csv'
+      puts archivo.read
+    end
   end
 end
